fecdd48efb33032f1b60f1707d38451148d25a9c
Jon Frederick <jonFred151@gmail.com>
2023-11-27 23:59:56 -0600

    Project finish

---
+++ SyntaxSugarExercise/SyntaxSugarExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SyntaxSugarExercise
+{
+     class program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+           var Num2 = 2;
+            Console.WriteLine("What is your favorite number?(Please put it in numerics)");
+            Num = int.Parse(Console.ReadLine());
+            
+
+            //Console.WriteLine(Num + FirstName + Petname);
+            var GreaterThanTwo = (Num > Num2) ? "Your favorite number is greater than 2!" : "Your number less than 2!";
+            Console.WriteLine( GreaterThanTwo);
+            
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
